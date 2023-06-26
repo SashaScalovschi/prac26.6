8704a69021eb77bfb52a1c40d1ec991df41eeec5
Aleksandryi <skalovskyi.oleksandr.clg@chnu.edu.ua>
2023-06-26 20:01:06 +0300

    нічого не допомогло

--- main.rb
+++ main.rb
@@ -1,13 +1,14 @@
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+# encoding: utf-8
+
 module Validation
-  MAX_NAME_LENGTH = 40
 
   def self.valid_name?(string)
-  #  return false if string.nil? || string.empty?
-  #  return false if string.length > MAX_NAME_LENGTH
 
     eng_alphabet = ("a".."z").to_a + ("A".."Z").to_a
     ua_alphabet = ("а".."я").to_a + ("А".."Я").to_a + ["і", "ї", "є", "ґ", "І", "Ї", "Є", "Ґ"]
-    valid_alphabet = eng_alphabet + ua_alphabet
+    valid_alphabet = ua_alphabet + eng_alphabet 
 
     return false if string.length > 40 || string.chars.any? { |c| !valid_alphabet.include?(c) }
 
@@ -15,8 +16,8 @@ module Validation
   end
 end
 
-puts "Введіть ім'я:"
-name = gets.chomp
+puts "Введіть ім'я (Важливе зауваження не використовуйте іменя які включають в собі укроїнський символ 'і'):"
+name = gets.chomp  #Замінити на "і" англійське 
 
 if Validation.valid_name?(name)
   puts "Ім'я '#{name}' є валідним."
