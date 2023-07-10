5c634802a919956e6cd5797f6fa0686d19cb0cca
Aleksandryi <skalovskyi.oleksandr.clg@chnu.edu.ua>
2023-07-10 12:56:53 +0300

    first person test

--- Task3/Person.rb
+++ Task3/Person.rb
@@ -1,82 +1,89 @@
-require 'date'
+require 'rspec'
+require_relative 'person'
 
-module ConsoleInput
-  def ask_user(prompt)
-    puts prompt
-    gets.chomp
-  end
-end
+RSpec.describe Person do
+  let(:valid_first_names) { ['Олександр', 'Іван', 'Марія', 'Олена', 'Вікторія'] }
+  let(:valid_last_name) { 'Ковальчук' }
+  let(:valid_inn) { '1234567890' }
+  let(:valid_birth_dates) { [Date.new(1990, 1, 1), Date.new(1985, 3, 15), Date.new(1993, 6, 30), Date.new(1988, 9, 12), Date.new(1997, 12, 25)] }
 
-module Validation
-  def valid_name?(name)
-    !name.nil? && !name.empty?
-  end
+  describe '#initialize' do
+    it 'creates a new instance of Person' do
+      person = Person.new(valid_first_names[0], valid_last_name, valid_inn, valid_birth_dates[0])
+      expect(person).to be_an_instance_of(Person)
+    end
 
-  def valid_inn?(inn)
-    /^\d{10}$/.match?(inn)
-  end
-
-  def valid_birth_date?(date)
-    date.is_a?(Date) && date <= Date.today
-  end
-end
-
-class Person
-  include ConsoleInput
-  include Validation
-
-  attr_reader :first_name, :last_name, :inn, :birth_date
-
-  def initialize(first_name, last_name, inn, birth_date)
-    self.first_name = first_name
-    self.last_name = last_name
-    self.inn = inn
-    self.birth_date = birth_date
-  end
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
-  def age
-    now = Date.today
-    now.year - birth_date.year - ((now.month > birth_date.month || (now.month == birth_date.month && now.day >= birth_date.day)) ? 0 : 1)
-  end
-
-  def to_s
-    "#{full_name}, INN: #{inn}, Birth Date: #{birth_date.strftime('%Y-%m-%d')}"
-  end
-
-  def first_name=(new_first_name)
-    if valid_name?(new_first_name)
-      @first_name = new_first_name
-    else
-      raise ArgumentError, "Invalid first name."
+    it 'raises an error when invalid arguments are provided' do
+      expect { Person.new(nil, valid_last_name, valid_inn, valid_birth_dates[0]) }.to raise_error(ArgumentError)
+      expect { Person.new(valid_first_names[0], nil, valid_inn, valid_birth_dates[0]) }.to raise_error(ArgumentError)
+      expect { Person.new(valid_first_names[0], valid_last_name, nil, valid_birth_dates[0]) }.to raise_error(ArgumentError)
+      expect { Person.new(valid_first_names[0], valid_last_name, valid_inn, 'invalid_date') }.to raise_error(ArgumentError)
     end
   end
 
-  def last_name=(new_last_name)
-    if valid_name?(new_last_name)
-      @last_name = new_last_name
-    else
-      raise ArgumentError, "Invalid last name."
+  describe '#full_name' do
+    it 'returns the full name of the person' do
+      person = Person.new(valid_first_names[1], valid_last_name, valid_inn, valid_birth_dates[1])
+      expect(person.full_name).to eq('Іван Ковальчук')
     end
   end
 
-  def inn=(new_inn)
-    if valid_inn?(new_inn)
-      @inn = new_inn
-    else
-      raise ArgumentError, "Invalid INN."
+  describe '#age' do
+    it 'returns the age of the person' do
+      person = Person.new(valid_first_names[2], valid_last_name, valid_inn, valid_birth_dates[2])
+      expect(person.age).to eq(Date.today.year - valid_birth_dates[2].year)
     end
   end
 
-  def birth_date=(new_birth_date)
-    birth_date = Date.parse(new_birth_date)
-    if valid_birth_date?(birth_date)
-      @birth_date = birth_date
-    else
-      raise ArgumentError, "Invalid birth date."
+  describe '#to_s' do
+    it 'returns a string representation of the person' do
+      person = Person.new(valid_first_names[3], valid_last_name, valid_inn, valid_birth_dates[3])
+      expect(person.to_s).to eq('Олена Ковальчук, INN: 1234567890, Birth Date: 1988-09-12')
+    end
+  end
+
+  describe 'attribute setters' do
+    let(:person) { Person.new(valid_first_names[4], valid_last_name, valid_inn, valid_birth_dates[4]) }
+
+    it 'sets the first name if it is valid' do
+      person.first_name = 'Наталія'
+      expect(person.first_name).to eq('Наталія')
+    end
+
+    it 'raises an error when setting an invalid first name' do
+      expect { person.first_name = nil }.to raise_error(ArgumentError)
+      expect { person.first_name = '' }.to raise_error(ArgumentError)
+    end
+
+    it 'sets the last name if it is valid' do
+      person.last_name = 'Петренко'
+      expect(person.last_name).to eq('Петренко')
+    end
+
+    it 'raises an error when setting an invalid last name' do
+      expect { person.last_name = nil }.to raise_error(ArgumentError)
+      expect { person.last_name = '' }.to raise_error(ArgumentError)
+    end
+
+    it 'sets the INN if it is valid' do
+      person.inn = '0987654321'
+      expect(person.inn).to eq('0987654321')
+    end
+
+    it 'raises an error when setting an invalid INN' do
+      expect { person.inn = nil }.to raise_error(ArgumentError)
+      expect { person.inn = '12345' }.to raise_error(ArgumentError)
+    end
+
+    it 'sets the birth date if it is valid' do
+      new_birth_date = Date.new(1995, 2, 15)
+      person.birth_date = new_birth_date
+      expect(person.birth_date).to eq(new_birth_date)
+    end
+
+    it 'raises an error when setting an invalid birth date' do
+      expect { person.birth_date = 'invalid_date' }.to raise_error(ArgumentError)
+      expect { person.birth_date = Date.today + 1 }.to raise_error(ArgumentError)
     end
   end
 end
