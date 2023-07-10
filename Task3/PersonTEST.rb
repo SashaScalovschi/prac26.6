259bc8967d1d07a2f991bf1ff4c7a90b3aee25a6
Aleksandryi <skalovskyi.oleksandr.clg@chnu.edu.ua>
2023-07-10 13:42:51 +0300

    last task3

--- Task3/PersonTEST.rb
+++ Task3/PersonTEST.rb
@@ -2,10 +2,10 @@ require 'rspec'
 require_relative 'person'
 
 RSpec.describe Person do
-  let(:valid_first_names) { ['Олександр', 'dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd', 'Марія', 'Олена', 'Вікторія'] }
-  let(:valid_last_names) { ['Ковальчук', 'Петренко', 'Сидоренко', 'Василенко', 'Ткаченко'] }
-  let(:valid_inns) { ['AZ1234543456', 'BG9876567890', 'CR2345RT4567', 'DT2346789', 'Et8765456789'] }
-  let(:valid_birth_dates) { [Date.new(1990, 1, 1), Date.new(1985, 3, 15), Date.new(1993, 6, 30), Date.new(1988, 9, 12), Date.new(1997, 12, 25)] }
+  let(:valid_first_names) { ['Олександр', 'ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd', 'Марія', 'Олена', 'Вікторія', 'Наталія', 'Петро'] }
+  let(:valid_last_names) { ['Мосайчук', 'Попов', 'Сидочук', 'Фрунзе', 'Коваль', 'Стерненко', 'Шевченко'] }
+  let(:valid_inns) { ['AZ1234543456', 'BG9876567890', 'Cf2345RT4567', 'DT23789', 'Et876sdf56789', 'HG98SD43210', 'UI12ffrgdbg90'] }
+  let(:valid_birth_dates) { [Date.new(1990, 1, 1), Date.new(1985, 3, 15), Date.new(1993, 6, 30), Date.new(1988, 9, 12), Date.new(1997, 12, 25), Date.new(1992, 4, 10), Date.new(1995, 2, 15)] }
 
   describe '#initialize' do
     it 'creates a new instance of Person' do
@@ -24,7 +24,7 @@ RSpec.describe Person do
   describe '#full_name' do
     it 'returns the full name of the person' do
       person = Person.new(valid_first_names[1], valid_last_names[1], valid_inns[1], valid_birth_dates[1])
-      expect(person.full_name).to eq('Іван Петренко')
+      expect(person.full_name).to eq('ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd Попов')
     end
   end
 
@@ -38,7 +38,7 @@ RSpec.describe Person do
   describe '#to_s' do
     it 'returns a string representation of the person' do
       person = Person.new(valid_first_names[3], valid_last_names[3], valid_inns[3], valid_birth_dates[3])
-      expect(person.to_s).to eq('Олена Василенко, INN: DT2345766789, Birth Date: 1988-09-12')
+      expect(person.to_s).to eq('Олена Фрунзе, INN: DT2346789, Birth Date: 1988-09-12')
     end
   end
 
@@ -56,8 +56,8 @@ RSpec.describe Person do
     end
 
     it 'sets the last name if it is valid' do
-      person.last_name = 'Ткаченко'
-      expect(person.last_name).to eq('Ткаченко')
+      person.last_name = 'Стерненко'
+      expect(person.last_name).to eq('Стерненко')
     end
 
     it 'raises an error when setting an invalid last name' do
@@ -66,9 +66,24 @@ RSpec.describe Person do
     end
 
     it 'sets the INN if it is valid' do
-      person.inn = 'ER9876543210'
-      expect(person.inn).to eq('ER9876543210')
+      person.inn = 'UI1234567890'
+      expect(person.inn).to eq('UI1234567890')
     end
 
-    it 'raiseі INN' do
-      expect { person.inn = nil }.to raise_error
+    it 'raises an error when setting an invalid INN' do
+      expect { person.inn = nil }.to raise_error(ArgumentError)
+      expect { person.inn = '12345' }.to raise_error(ArgumentError)
+    end
+
+    it 'sets the birth date if it is valid' do
+      new_birth_date = Date.new(1995, 2, 15)
+      person.birth_date = new_birth_date
+      expect(person.birth_date).to eq(new_birth_date)
+    end
+
+    it 'raises an error when setting an invalid birth date' do
+      expect { person.birth_date = 'invalid_date' }.to raise_error(ArgumentError)
+      expect { person.birth_date = Date.today + 1 }.to raise_error(ArgumentError)
+    end
+  end
+end
